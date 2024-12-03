7b8a973d889ae5e5465cdea57e8380d27f348732
algGirona <alg.girona1@gmail.com>
2024-12-03 20:04:00 +0100

    This file is Hello Word

---
+++ HelloWord.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+
+public class HelloWord : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Hola");
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+
+    
+
+
